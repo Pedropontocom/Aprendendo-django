de98bcb458ebce6bc65581676fd098d28df97936
Pedropontocom <121833115+Pedropontocom@users.noreply.github.com>
2024-08-21 08:31:26 -0300

    Add files via upload

--- django/hello/galeria/views.py
+++ django/hello/galeria/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 # from django.http import HttpResponse
 
 def index(request):
-    return render(request, "index.html")
+    fotos = Fotografia.objects.all()
+    return render(request, "index.html", {"fotos": fotos})
     # return HttpResponse("<h1>Hello World</h1>")
